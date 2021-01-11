251661c494cec5f174fc872b8a9f0570b8053a62
alexander <alexander_holm@hotmail.com>
2021-01-11 08:05:42 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomePageLayout } from '../layouts/HomePage';
+
+export const Home = () => {
+  return (
+    <HomePageLayout>
+      <h1>Hello from homepage</h1>
+    </HomePageLayout>
+  );
+};
